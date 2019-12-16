d4271b0ed959c23e7237b765500b6cc7cc16b6d5
tardyparty <tardyparty@users.noreply.github.com>
2019-12-16 00:41:39 -0600

    getting ready for api and mongo

--- client/src/pages/search.js
+++ client/src/pages/search.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import API from "../utils/API";
 import { Link } from "react-router-dom";
 import Jumbo from "../components/Jumbo"
+import { Container } from "react-bootstrap";
 
 export default class Search extends Component {
 
@@ -11,7 +12,12 @@ export default class Search extends Component {
 
   render() {
     return(
-      <Jumbo/>
+      <div>
+        <Jumbo/>
+        <Container>
+          {}
+        </Container>
+      </div>
     )
   }
 }
